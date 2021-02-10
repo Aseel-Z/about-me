a3b52b075202fc1a39407f8eb813a3986bf81201
Aseel-Z <aseelalzweri@gmail.com>
2021-02-10 18:01:04 +0200

    Version01

--- js/app.js
+++ js/app.js
@@ -42,22 +42,22 @@ while(attemptsNo){
   }
 }
 
-alert('Now answer with yes[y] or no[n] please');
-
 let myJob = prompt('Am I an engineer?');
 while (myJob !== 'yes' && myJob !== 'y' && myJob !== 'no' && myJob !== 'n') {
   myJob = prompt('Am I an engineer? yes[y] or no[n]?');
 }
-if (myJob.toLowerCase() === 'yes' || myJob.toLowerCase() === 'y') {
-  alert('You are Correct!');
-  correctCounter++;
-  //  console.log('Correct');
-} else if (myJob.toLowerCase() === 'no' || myJob.toLowerCase() === 'n') {
-  alert('You are not Correct, check my info below!');
+function job (myJob){
+  if (myJob.toLowerCase() === 'yes' || myJob.toLowerCase() === 'y') {
+    alert('You are Correct!');
+    correctCounter++;
+    //  console.log('Correct');
+  } else if (myJob.toLowerCase() === 'no' || myJob.toLowerCase() === 'n') {
+    alert('You are not Correct, check my info below!');
+  }
 }
-
+job ();
 let isMom = prompt('Am I a mother?');
-while (isMom !== 'yes' && isMom !== 'y' && isMom !== 'no' && isMom !== 'n') {
+function mom(isMom){while (isMom !== 'yes' && isMom !== 'y' && isMom !== 'no' && isMom !== 'n') {
   isMom = prompt('Am I a mother? yes[y] or no[n]?');
 }
 if (isMom.toLowerCase() === 'yes' || isMom.toLowerCase() === 'y') {
@@ -67,42 +67,52 @@ if (isMom.toLowerCase() === 'yes' || isMom.toLowerCase() === 'y') {
 } else if (isMom.toLowerCase() === 'no' || isMom.toLowerCase() === 'n') {
   alert('You are not Correct, check my info below!');
 }
+}
+mom();
 
 let myPet = prompt('Do you think my pet is a dog?');
 while (myPet !== 'yes' && myPet !== 'y' && myPet !== 'no' && myPet !== 'n') {
   myPet = prompt('do you think my pet is a dog? yes[y] or no[n]?');
 }
+function pet (myPet){
 if (myPet.toLowerCase() === 'yes' || myPet.toLowerCase() === 'y') {
   alert('You are Correct!');
   correctCounter++;
   //  console.log('Correct');
 } else if (myPet.toLowerCase() === 'no' || myPet.toLowerCase() === 'n') {
   alert('You are not Correct, check my info below!');
-}
+}}
+pet();
 
 let myCity = prompt('Do you think I live in Amman?');
 while (myCity !== 'yes' && myCity !== 'y' && myCity !== 'no' && myCity !== 'n') {
   myCity = prompt('Do you think I live in Amman? yes[y] or no[n]?');
 }
-if (myCity.toLowerCase() === 'yes' || myCity.toLowerCase() === 'y') {
-  alert('You are Correct!');
-  correctCounter++;
-  //  console.log('Correct');
-} else if (myCity.toLowerCase() === 'no' || myCity.toLowerCase() === 'n') {
-  alert('You are not Correct, check my info below!');
+function city(myCity){
+  if (myCity.toLowerCase() === 'yes' || myCity.toLowerCase() === 'y') {
+    alert('You are Correct!');
+    correctCounter++;
+    //  console.log('Correct');
+  } else if (myCity.toLowerCase() === 'no' || myCity.toLowerCase() === 'n') {
+    alert('You are not Correct, check my info below!');
+  }
 }
+city();
 
 let myHobby = prompt('Do you think I played Taekwondo?');
 while (myHobby !== 'yes' && myHobby !== 'y' && myHobby !== 'no' && myHobby !== 'n') {
   myHobby = prompt('Do you think I played Taekwondo? yes[y] or no[n]?');
 }
-if (myHobby.toLowerCase() === 'yes' || myHobby.toLowerCase() === 'y') {
-  alert('You are Correct!');
-  correctCounter++;
-  //  console.log('Correct');
-} else if (myHobby.toLowerCase() === 'no' || myHobby.toLowerCase() === 'n') {
-  alert('You are not Correct, check my info below!');
+function hobby (myHobby){
+  if (myHobby.toLowerCase() === 'yes' || myHobby.toLowerCase() === 'y') {
+    alert('You are Correct!');
+    correctCounter++;
+    //  console.log('Correct');
+  } else if (myHobby.toLowerCase() === 'no' || myHobby.toLowerCase() === 'n') {
+    alert('You are not Correct, check my info below!');
+  }
 }
+hobby();
 
 alert('Welcome ' + userName);
 alert('You scored ' + correctCounter + ' Out of 7!');
