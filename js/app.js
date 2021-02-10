12fc773d2bb5dfa0a9b1e5bc19ea74cfdb975633
MaisDaoud <eng.omosh@gmail.com>
2021-02-10 09:56:10 +0200

    lab03

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 'use strict';
 
-let userInput2 = prompt('Guess one of my Top Ten Places to Visit');
-let favCity=['Prague','Paris','Berlin','Dublin','Stockholm','Moscow','Madrid7','Oslo','London', 'Sofia'];
-let attemptsNo = 6;
-while (attemptsNo) {
-  if (String(userInput2.toLowerCase()) === favCity) {
+let correctCounter=0;
+
+let attemptsNo=6;
+while(attemptsNo){
+  let userInput2 = prompt('Guess one of my Top Ten Places to Visit!').toLowerCase();
+  if ((userInput2=== 'prague') || (userInput2=== 'paris') || (userInput2=== 'berlin') || (userInput2=== 'dublin') || (userInput2=== 'stockholm') || (userInput2=== 'moscow') || (userInput2=== 'madrid') || (userInput2=== 'oslo') || (userInput2=== 'london') || (userInput2=== 'sofia')){
     alert('Correct!! ' + userInput2 + ' is one of my favorite cities.');
+    correctCounter++;
     break;
-  } else {
+  } else{
     attemptsNo--;
     alert('No, Try again, you still have : ' + attemptsNo + ' attempts remaining!');
-  }
-  userInput2 = prompt('Guess one of my Top 10 Places to Visit');
-  if (attemptsNo===1) {
-    alert('Game Over! check my info below!');
-    break;
+    if(!attemptsNo){
+      alert('Game Over! check my info below!');
+      break;
+    }
   }
 }
 
@@ -26,6 +27,7 @@ let attempts = 4;
 while (attempts) {
   if (Number(userInput) === myFavNum) {
     alert('Correct!! 7 is my favorite number.');
+    correctCounter++;
     break;
   } else if (Number(userInput) > myFavNum) {
     attempts--;
@@ -49,6 +51,7 @@ while (myJob !== 'yes' && myJob !== 'y' && myJob !== 'no' && myJob !== 'n') {
 }
 if (myJob.toLowerCase() === 'yes' || myJob.toLowerCase() === 'y') {
   alert('You are Correct!');
+  correctCounter++;
   //  console.log('Correct');
 } else if (myJob.toLowerCase() === 'no' || myJob.toLowerCase() === 'n') {
   alert('You are not Correct, check my info below!');
@@ -60,6 +63,7 @@ while (isMom !== 'yes' && isMom !== 'y' && isMom !== 'no' && isMom !== 'n') {
 }
 if (isMom.toLowerCase() === 'yes' || isMom.toLowerCase() === 'y') {
   alert('You are Correct!');
+  correctCounter++;
   //  console.log('Correct');
 } else if (isMom.toLowerCase() === 'no' || isMom.toLowerCase() === 'n') {
   alert('You are not Correct, check my info below!');
@@ -71,6 +75,7 @@ while (myPet !== 'yes' && myPet !== 'y' && myPet !== 'no' && myPet !== 'n') {
 }
 if (myPet.toLowerCase() === 'yes' || myPet.toLowerCase() === 'y') {
   alert('You are Correct!');
+  correctCounter++;
   //  console.log('Correct');
 } else if (myPet.toLowerCase() === 'no' || myPet.toLowerCase() === 'n') {
   alert('You are not Correct, check my info below!');
@@ -82,6 +87,7 @@ while (myCity !== 'yes' && myCity !== 'y' && myCity !== 'no' && myCity !== 'n') 
 }
 if (myCity.toLowerCase() === 'yes' || myCity.toLowerCase() === 'y') {
   alert('You are Correct!');
+  correctCounter++;
   //  console.log('Correct');
 } else if (myCity.toLowerCase() === 'no' || myCity.toLowerCase() === 'n') {
   alert('You are not Correct, check my info below!');
@@ -93,75 +99,14 @@ while (myHobby !== 'yes' && myHobby !== 'y' && myHobby !== 'no' && myHobby !== '
 }
 if (myHobby.toLowerCase() === 'yes' || myHobby.toLowerCase() === 'y') {
   alert('You are Correct!');
+  correctCounter++;
   //  console.log('Correct');
 } else if (myHobby.toLowerCase() === 'no' || myHobby.toLowerCase() === 'n') {
   alert('You are not Correct, check my info below!');
 }
 
 alert('Welcome ' + userName);
+alert('Your Score' + correctCounter);
 
 document.getElementById('Name').innerHTML = userName;
-
-///////////////////////////////////
-
-
-
-
-// console.log(numList.length);
-// console.log(numList);
-
-// let attemptNo=6;
-// let userInput2=prompt('Guess my favorite number between 1 and 9');
-// while(attemptNo)
-// {
-//   for (let i= 0;i<attempts;i++)
-//   {
-//     if (userInput2==='7')
-//     {
-//       alert('Correct!! 7 is my favorite number.');
-//       correct=true;
-//       break;
-//     }
-//     attemptNo-=1;
-//     userInput2=prompt('Guess my favorite number between 1 and 9');
-//     if (userInput2>'7'){
-//       alert('Too High, Try again, you still have : '+attemptNo+' attempts remaining!');
-//       break;
-//     }else
-//     {
-//       alert('Too Low, Try again, you still have : '+attemptNo+' attempts remaining!');
-//     }
-//   }
-
-//   if (!attemptNo)
-//   {
-//     alert('The correct number is 7!');
-//     break;
-//   }
-// }
-
-
-// As a user, I would like to know my final score so that I can know how well I did.
-// Keep track of the total number of correct answers. 
-//At the end tell them how many they got correct out of the 7 questions asked.
-
-// var randomNumber = getRandomNumber(10);
-// var guess;
-// var guessCount = 0;
-// var correctGuess = false;
-
-// function getRandomNumber(upper) {
-//   var num = Math.floor(Math.random() * upper) + 1;
-//   return num;
-// }
-
-// do {
-//   guess = prompt('I am thinking of a number between 1 and 10. What is it?');
-//   guessCount += 1;
-//   if (parseInt(guess) === randomNumber) {
-//     correctGuess = true;
-// }
-
-// } while( !correctGuess )
-//   document.write('You guessed the number!');
-//   document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber + '.');
+document.getElementById('correctCount').innerHTML = correctCounter;
